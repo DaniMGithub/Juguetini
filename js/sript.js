51d575991e1c890133bf474602f123e32f3b58de
Dani Milano <danimilano6@gmail.com>
2023-04-27 18:44:36 -0300

    mailjs error

--- js/sript.js
+++ js/sript.js
@@ -1,11 +1,13 @@
 /* Mail para el formulario de contacto realizado con emailjs.com */
 
 function SendMail(){
-  var params = {
+  const params = {
   from_name : document.getElementById("fullName").value,
   email_id : document.getElementById("email_id").value,
   message : document.getElementById("message").value,
   }
-  emailjs.send("service")
+  emailjs.send("service_1klskos", "template_hn0nrzr", params).then(function(res) {
+    alert("exito" +res.status);
+  })
   }
 
