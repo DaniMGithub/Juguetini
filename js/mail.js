4a805998ba06e0023081db872856c2374890ad89
Dani Milano <danimilano6@gmail.com>
2023-05-08 01:54:16 -0300

    email.js

---
+++ js/mail.js
@@ -0,0 +1,22 @@
+/* Mail para el formulario de contacto realizado con emailjs.com */
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_hn0nrzr';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
